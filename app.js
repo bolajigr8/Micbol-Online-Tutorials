0cd59d42347556bb29d8b3384d154caec04bb817
Bolaji Michael Adebayo <144087782+bolajigr8@users.noreply.github.com>
2024-02-12 16:21:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 // setup date
-const date = (document.getElementById(
-  "date"
-).innerHTML = new Date().getFullYear());
+const date = (document.getElementById("date").innerHTML =
+  new Date().getFullYear());
 // setup nav
 const navBtn = document.getElementById("nav-btn");
 const navbar = document.getElementById("navbar");
 const navClose = document.getElementById("nav-close");
 // show nav
 navBtn.addEventListener("click", () => {
-  navbar.classList.add("showNav");
+  navbar.classList.toggle("showNav");
 });
 // close nav
 navClose.addEventListener("click", () => {
